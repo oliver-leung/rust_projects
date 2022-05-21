a1991cfa3f7ba8c758a1e150ddabbfcd2d6eb7b4
Oliver Leung <oliverleung2@gmail.com>
2022-05-21 13:45:16 -0400

    Start on ownership

---
+++ ownership/src/main.rs
@@ -0,0 +1,35 @@
+fn main() {
+    move_example();
+    take_vs_make();
+}
+
+fn move_example() {
+    {
+        let mut s = String::from("hello");
+        s.push_str(", world!");
+        println!("{}", s);
+    }
+    {
+        let s1 = String::from("hello");
+        let s2 = s1.clone();
+        println!("s1 = {}, s2 = {}", s1, s2);
+    }
+}
+
+fn take_vs_make() {
+    let mut s = String::from("hello");
+    s = takes_ownership(s);
+
+    let x = 5;
+    makes_copy(x);
+    // x is still usable
+}
+
+fn takes_ownership(some_string: String) -> String {
+    println!("{}", some_string);
+    some_string
+}
+
+fn makes_copy(some_int: i32) {
+    println!("{}", some_int)
+}
