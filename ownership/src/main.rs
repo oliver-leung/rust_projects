4d45783088acb3a14f37404695ec33cfda1f0f25
Oliver Leung <oliverleung2@gmail.com>
2024-07-04 13:42:06 -0500

    Add ownership module, update .gitignore and hello_rust/main binary

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
-    move_example();
-    take_vs_make();
+    // move_example();
+    // take_vs_make();
+    references();
 }
 
 fn move_example() {
@@ -32,4 +33,14 @@ fn takes_ownership(some_string: String) -> String {
 
 fn makes_copy(some_int: i32) {
     println!("{}", some_int)
+}
+
+fn references() {
+    let s1 = String::from("hello");
+    let len = calculate_length(&s1);
+    println!("The length of {} is {}.", s1, len)
+}
+
+fn calculate_length(s: &String) -> usize {
+    s.len()
 }
